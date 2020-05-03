bc3b9c66e0294a47febee5f358af079a4a73ac50
asus <chen@asus.com>
2020-05-03 18:59:37 +0800

    生成V2_02相对误差结果图

--- euroc_results/copy_gt_est.sh
+++ euroc_results/copy_gt_est.sh
@@ -5,5 +5,10 @@
 #########################################################################
 #!/bin/bash
 
-cp ../../../02-VIO/01-shenlan/05-chapter7_VINS_Course/my_vins/euroc_data/MH_05_difficult/groundtruth.tum ./MH_05/stamped_groundtruth.txt
-cp ../../../02-VIO/01-shenlan/05-chapter7_VINS_Course/my_vins/bin/estimate_result.txt ./MH_05/stamped_traj_estimate.txt
+# my_vins
+# cp ../../../02-VIO/01-shenlan/05-chapter7_VINS_Course/my_vins/euroc_data/MH_05_difficult/groundtruth.tum ./MH_05/stamped_groundtruth.txt
+# cp ../../../02-VIO/01-shenlan/05-chapter7_VINS_Course/my_vins/bin/estimate_result.txt ./MH_05/stamped_traj_estimate.txt
+
+# Remove_ROS_VINS
+cp ../../../02-VIO/01-shenlan/05-chapter7_VINS_Course/Remove_ROS_VINS/results/groundtruth_V2_02.tum ./V2_02/stamped_groundtruth.txt
+cp ../../../02-VIO/01-shenlan/05-chapter7_VINS_Course/Remove_ROS_VINS/results/estimate_result_V2_02.txt ./V2_02/stamped_traj_estimate.txt
